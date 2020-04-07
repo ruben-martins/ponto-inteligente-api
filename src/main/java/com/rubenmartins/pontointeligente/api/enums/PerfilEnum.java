1b814de643e05c29bd0c70483409f77b965313b2
ruben-martins <rubenhlm@gmail.com>
2020-04-07 16:59:18 -0300

    entidades

--- src/main/java/com/rubenmartins/pontointeligente/api/enums/PerfilEnum.java
+++ src/main/java/com/rubenmartins/pontointeligente/api/enums/PerfilEnum.java
@@ -1,4 +1,6 @@
 package com.rubenmartins.pontointeligente.api.enums;
 
 public enum PerfilEnum {
+    ROLE_ADMIN,
+    ROLE_USUARIO
 }
--- src/main/java/com/rubenmartins/pontointeligente/api/enums/TipoEnum.java
+++ src/main/java/com/rubenmartins/pontointeligente/api/enums/TipoEnum.java
@@ -1,4 +1,10 @@
 package com.rubenmartins.pontointeligente.api.enums;
 
-public class TipoEnum {
+public enum TipoEnum {
+    INICIO_TRABALHO,
+    TERMINO_TRABALHO,
+    INICIO_ALMOCO,
+    TERMINO_ALMOCO,
+    INICIO_PAUSA,
+    TERMINO_PAUSA
 }
