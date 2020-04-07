acb45a27d38fe947eaba581bb2db75ffe70962d4
ruben-martins <rubenhlm@gmail.com>
2020-04-07 17:22:15 -0300

    criando repositories e profile de teste

---
+++ src/main/java/com/rubenmartins/pontointeligente/api/repositories/EmpresaRepository.java
@@ -0,0 +1,11 @@
+package com.rubenmartins.pontointeligente.api.repositories;
+
+import com.rubenmartins.pontointeligente.api.entities.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+
+    @Transactional(readOnly = true)
+    Empresa findByCnpj();
+}
